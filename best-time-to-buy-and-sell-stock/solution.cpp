7fec3632bcc4ac9b6f5e59ca6de07050f317d2a0
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2017-06-25 14:51:46 +0000

    Sync LeetCode submission Runtime - 6 ms, Memory - N/A

--- best-time-to-buy-and-sell-stock/solution.cpp
+++ best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-      int n = prices.size();
-      if (n == 0) return 0;
-      int ans = INT_MIN;
-      int mn = prices[0];
-      for (int i = 1; i < n; ++i) {
-          if ((prices[i] - mn) > ans)
-              ans = prices[i] - mn;
-          mn = min(prices[i], mn);
-      }
-      return ans > 0 ? ans : 0;
+        if(prices.size() == 0) return 0;
+        vector<int> dp(prices.size(), 0);
+        int mx = prices[prices.size()-1];
+        for (int i = prices.size()-2; i >= 0; --i) {
+            mx = max(mx, prices[i]);
+            dp[i] = max(dp[i+1], mx-prices[i]);
+        }
+        return *max_element(dp.begin(), dp.end());
     }
 };
