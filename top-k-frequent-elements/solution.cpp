80179cd075f82d95867e3b0a0daf8ac9380a1296
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2018-03-20 09:09:40 +0000

    Sync LeetCode submission Runtime - 22 ms, Memory - N/A

---
+++ top-k-frequent-elements/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> cnt;
+        for(auto &x : nums)
+            cnt[x]++;
+        vector<vector<int> > bkt(nums.size()+1);
+        for(auto &x: cnt) {
+            bkt[x.second].push_back(x.first);
+        }
+        vector<int> res;
+        for (int i = nums.size(); i >= 1; --i) {
+            for(auto x: bkt[i]) {
+                res.push_back(x);
+                if(res.size() == k) return res;
+            }
+        }
+        return res;
+    }
+};
