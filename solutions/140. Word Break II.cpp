97e41c65d28f00ad9158034b87da1765be84843c
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2021-04-19 23:18:18 +0530

    12 ms

---
+++ solutions/140. Word Break II.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    unordered_set<string> dict;
+unordered_map<int, vector<string> > dp;
+int n;
+vector<string> f(string s, int i) {
+    if(i == n) {
+        vector<string> curr;
+        curr.push_back("");
+        return curr;
+    }
+    if(dp.find(i) != dp.end())
+        return dp[i];
+    string temp;
+    vector<string> curr;
+    for(int l = 1; i+l <= n; ++l) {
+        temp = s.substr(i, l);
+        if(dict.find(temp) != dict.end()) {
+            auto x = f(s, i+l);
+            for(auto &y: x) {
+                if(y != "")
+                    curr.push_back(temp+" "+y);
+                else 
+                    curr.push_back(temp);
+            }
+        }
+    }
+    return dp[i] = curr;
+}
+vector<string> wordBreak(string A, vector<string> &B) {
+    for(auto &x: B) 
+        dict.insert(x);
+    n = A.size();
+    dp.clear();
+    auto ans = f(A, 0);
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+​
+};
