17363955d99bade358e85c99927d6de0f2133a76
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2021-04-19 23:16:37 +0530

    N/A

---
+++ solutions/523. Continuous Subarray Sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+         unordered_map<int, bool> prefix_sum;
+         int n = nums.size();
+         if (n <= 1) return false;         
+         if(k == 0) {
+             for (int i = 1; i < n; ++i) {
+                 if(!nums[i] and !nums[i-1])
+                     return true;
+                 else return false;
+             }
+         }
+         prefix_sum[nums[0]%k] = 0;
+         int sum = nums[0]; 
+         for (int i = 1; i < n; ++i) {
+             sum += nums[i];
+             int n = sum%k;
+             if(n == 0){
+                 return true;
+             }
+             if(prefix_sum.find(n) != prefix_sum.end()) {
+                 if(i-prefix_sum[n] >= 2) return true;                
+             }
+             else {
+                 prefix_sum[n] = i;
+             }             
+         }
+        return false;
+    }
+};
