0573a94d27297890abf510c5bee212352ab92416
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-01-10 04:29:28 +0000

    Sync LeetCode submission - The Number of Weak Characters in the Game (python3)

---
+++ solutions_new/problems/the_number_of_weak_characters_in_the_game/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
+        properties.sort(key=lambda x: (-x[0], x[1]))
+        ans = max_defence = 0
+        for x in properties:
+            if x[1] < max_defence:
+                ans += 1
+            else:
+                max_defence = max(max_defence, x[1])
+        return ans
