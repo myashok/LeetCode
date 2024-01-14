c1ad84a6988319cb0db2b5971b60b59e799e33cc
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-01-14 11:58:48 +0000

    Sync LeetCode submission - Sort Colors (python3)

---
+++ problems/sort_colors/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count = Counter(nums)
+        nums.clear()  
+        for element, frequency in sorted(count.items()):
+            nums.extend([element] * frequency)
