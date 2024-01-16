aa0ff0f4ec335b6978d9c159e4bae6f81c43a6a9
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-01-16 11:44:50 +0000

    Sync LeetCode submission Runtime - 275 ms (96.50%), Memory - 37.2 MB (9.91%)

---
+++ 0041-first-missing-positive/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = set(nums)
+        i = 1
+        while i in nums:
+            i += 1
+        return i
+
