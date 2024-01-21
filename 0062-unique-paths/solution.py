1120ba67a9960b01c41d1fc1437ce9d2ddf76292
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-01-21 11:16:55 +0000

    Sync LeetCode submission Runtime - 34 ms (82.13%), Memory - 16.5 MB (60.84%)

---
+++ 0062-unique-paths/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if n==1 or m == 1:
+            return 1
+                   
+        dp = [[1]*n for _ in range(m)]
+                
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        return dp[m-1][n-1]
