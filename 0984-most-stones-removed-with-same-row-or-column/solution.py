a15fd4f8eff236917790b0f937551975cf74758d
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-02-08 17:38:26 +0000

    Sync LeetCode submission Runtime - 111 ms (98.19%), Memory - 17.4 MB (73.95%)

---
+++ 0984-most-stones-removed-with-same-row-or-column/solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    class UnionFind:
+        def __init__(self):
+            self.parent = {}
+            self.rank = defaultdict(int)
+
+        def find(self, x):
+            if x != self.parent.setdefault(x, x):
+                self.parent[x] = self.find(self.parent[x])
+            return self.parent[x]
+
+        def union(self, x, y):
+            root_x, root_y = self.find(x), self.find(y)
+            if root_x != root_y:
+                if self.rank[root_x] < self.rank[root_y]:
+                    self.parent[root_x] = root_y
+                elif self.rank[root_x] > self.rank[root_y]:
+                    self.parent[root_y] = root_x
+                else:
+                    self.parent[root_y] = root_x
+                    self.rank[root_x] += 1
+
+        def get_disjoint_sets_count(self):
+            return sum(1 for k, v in self.parent.items() if k == v)
+
+
+    def removeStones(self, stones):
+        uf = self.UnionFind()
+        for x, y in stones:
+            uf.union(x, ~y)
+        return len(stones) - uf.get_disjoint_sets_count()
+
