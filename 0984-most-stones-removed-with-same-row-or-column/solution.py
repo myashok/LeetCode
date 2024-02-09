e5d9b7be2ca00b5543d4b05d8ab62c2ab23160c7
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-02-09 04:28:55 +0000

    Sync LeetCode submission Runtime - 121 ms (91.18%), Memory - 17.3 MB (82.36%)

--- 0984-most-stones-removed-with-same-row-or-column/solution.py
+++ 0984-most-stones-removed-with-same-row-or-column/solution.py
@@ -5,20 +5,26 @@ class Solution:
             self.rank = defaultdict(int)
 
         def find(self, x):
-            if x != self.parent.setdefault(x, x):
+            if x not in self.parent:
+                self.parent[x] = x
+                
+            if x != self.parent[x]:
                 self.parent[x] = self.find(self.parent[x])
+            
             return self.parent[x]
 
         def union(self, x, y):
             root_x, root_y = self.find(x), self.find(y)
-            if root_x != root_y:
-                if self.rank[root_x] < self.rank[root_y]:
-                    self.parent[root_x] = root_y
-                elif self.rank[root_x] > self.rank[root_y]:
-                    self.parent[root_y] = root_x
-                else:
-                    self.parent[root_y] = root_x
-                    self.rank[root_x] += 1
+            
+            if root_x == root_y:
+                return 
+            
+            if self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+                self.rank[root_y] += 1
+            else:
+                self.parent[root_y] = root_x
+                self.rank[root_x] += 1
 
         def get_disjoint_sets_count(self):
             return sum(1 for k, v in self.parent.items() if k == v)
