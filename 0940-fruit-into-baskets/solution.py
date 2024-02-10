de5f38e9321e09aad1a5be35d4d10352e81ae2ad
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-02-10 16:21:49 +0000

    Sync LeetCode submission Runtime - 604 ms (92.03%), Memory - 24.7 MB (42.96%)

--- 0940-fruit-into-baskets/solution.py
+++ 0940-fruit-into-baskets/solution.py
@@ -1,24 +1,12 @@
 class Solution:
-    def totalFruit(self, fruits: List[int]) -> int:
-        l , ans, picked_type = 0, 1, dict() 
-        for i, fruit in enumerate(fruits):
-            if fruit in picked_type:
-                ans = max(ans, i - l + 1)
-            else:
-                if len(picked_type) < 2:
-                    ans = max(ans, i - l + 1)
-                else:
-                    (key1, value1), (key2, value2) = picked_type.items()
-                    if value1 < value2:
-                        value_of_jump = value1
-                        picked_type.pop(key1)
-                    else:
-                        value_of_jump = value2
-                        picked_type.pop(key2)
-                    l = value_of_jump + 1
-            picked_type[fruit] = i
-
-        return ans
-
+    def totalFruit(self, tree: List[int]) -> int:
+        count, i = {}, 0
+        for j, v in enumerate(tree):
+            count[v] = count.get(v, 0) + 1
+            if len(count) > 2:
+                count[tree[i]] -= 1
+                if count[tree[i]] == 0: del count[tree[i]]
+                i += 1
+        return j - i + 1
             
 
