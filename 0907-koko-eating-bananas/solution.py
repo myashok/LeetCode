adfb2bd148846b6bd82efd8b19d95c0fa2c461ad
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-02-10 10:27:32 +0000

    Sync LeetCode submission Runtime - 244 ms (95.93%), Memory - 18.2 MB (73.71%)

---
+++ 0907-koko-eating-bananas/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], r: int) -> int:
+        l, h = 1, max(piles)
+        
+        def _is_this_capacity_possibile(k: int) -> bool:
+            hours = 0
+            for pile in piles:
+                hours += ceil(pile/k)
+
+            return hours <= r
+
+
+        while l < h:
+            mid = (l + h) // 2
+            if _is_this_capacity_possibile(mid):
+                h = mid
+            else:
+                l = mid + 1
+
+        return l
