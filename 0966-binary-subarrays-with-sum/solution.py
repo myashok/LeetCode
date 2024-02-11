72fe7252bb8086a64abd46e6bf527364917a10d7
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-02-11 17:08:07 +0000

    Sync LeetCode submission Runtime - 217 ms (79.35%), Memory - 17.5 MB (87.74%)

---
+++ 0966-binary-subarrays-with-sum/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        i = count = res = 0
+        for j in range(len(nums)):
+            if nums[j] == 1:
+                goal -= 1
+                count = 0
+            while goal < 0:
+                goal += nums[i]
+                i += 1
+
+            while goal == 0 and i <= j:
+                goal += nums[i]
+                i += 1
+                count += 1
+                if i > j - goal + 1:
+                    break
+
+            res += count
+        return res
