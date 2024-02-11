35ddf9364b1552c34b59bc0aa2f28dfed5b550c8
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-02-11 13:36:52 +0000

    Sync LeetCode submission Runtime - 552 ms (98.66%), Memory - 23.7 MB (73.87%)

---
+++ 1370-count-number-of-nice-subarrays/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        l = ans = odd_count = count = 0
+        for num in nums:
+            if num & 1:
+                odd_count += num & 1
+                count = 0
+            while odd_count == k:
+                odd_count -= nums[l] & 1
+                count += 1
+                l += 1
+            ans += count
+        return ans
+
+
