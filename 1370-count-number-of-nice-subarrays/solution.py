f45727f5f53fcdb19cbd6f8d236a623ca04c8688
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-02-11 13:43:49 +0000

    Sync LeetCode submission Runtime - 566 ms (92.84%), Memory - 23.6 MB (73.87%)

--- 1370-count-number-of-nice-subarrays/solution.py
+++ 1370-count-number-of-nice-subarrays/solution.py
@@ -1,15 +1,18 @@
+from typing import List
+
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         l = ans = odd_count = count = 0
         for num in nums:
+            # If the number is odd, increment the odd_count
             if num & 1:
-                odd_count += num & 1
-                count = 0
+                odd_count += 1
+                count = 0  # Reset the count for consecutive subarrays with k odd numbers
             while odd_count == k:
-                odd_count -= nums[l] & 1
-                count += 1
-                l += 1
-            ans += count
+                # If odd_count equals k, we found a valid subarray
+                odd_count -= nums[l] & 1  # Decrease odd_count when we move the left pointer
+                count += 1  # Increase count for each valid subarray
+                l += 1  # Move the left pointer to the right to find more subarrays
+            ans += count  # Update the total count of subarrays
         return ans
-
 
