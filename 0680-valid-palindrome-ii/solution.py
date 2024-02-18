9054c5b858c396e620446a03b1bf625acdb9fc32
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-02-18 12:27:07 +0000

    Sync LeetCode submission Runtime - 123 ms (22.75%), Memory - 24.3 MB (5.04%)

---
+++ 0680-valid-palindrome-ii/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        if s == s[::-1]: 
+            return True
+        def can_b_valid_palin(i, j, deleted):
+            if deleted > 1:
+                return False
+
+            if i > j:
+                return True
+
+            if s[i] != s[j]:
+                return can_b_valid_palin(i+1, j, deleted+1) or can_b_valid_palin(i, j-1, deleted+1)
+            
+            return  can_b_valid_palin(i+1, j-1, deleted)
+
+        return can_b_valid_palin(0, len(s)-1, 0)
