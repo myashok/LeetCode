0f5be838527195982c507a13f96c9cacf4c1cfb1
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-02-19 07:08:19 +0000

    Sync LeetCode submission Runtime - 1926 ms (18.73%), Memory - 87.2 MB (42.13%)

---
+++ 2409-number-of-increasing-paths-in-a-grid/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countPaths(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        position_val = defaultdict(list)
+        max_path_val = [[0]*n for _ in range(m)]
+        
+        for i in range(m):
+            for j in range(n):
+                position_val[grid[i][j]].append((i, j))
+        
+        direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        def is_safe(i, j):
+            return 0 <= i < m and 0 <= j < n
+        
+        ans = 0
+        mod = (10**9 + 7)
+        for val in sorted(position_val, reverse=True):
+            for row, col in position_val[val]:
+                temp = 1
+                for dx, dy in direction:
+                    new_row = row + dx
+                    new_col = col + dy
+                    if is_safe(new_row, new_col) and grid[new_row][new_col] > grid[row][col]:
+                        temp += max_path_val[new_row][new_col]
+                ans += temp
+                max_path_val[row][col] = temp
+            ans %= (mod)
+        return ans % (mod)
