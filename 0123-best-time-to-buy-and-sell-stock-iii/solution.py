ce4e0ce0ef516a3cbb27f919a7cbe7aac6d0d7ed
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-02-23 06:59:26 +0000

    Sync LeetCode submission Runtime - 2024 ms (16.20%), Memory - 602.5 MB (5.04%)

--- 0123-best-time-to-buy-and-sell-stock-iii/solution.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/solution.py
@@ -1,18 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        if n <= 1:
-            return 0
-        dp = [0]*n
-        prev_min = prices[0]
-        for i in range(1,n):
-            dp[i] = max(0, prices[i] - prev_min, dp[i-1])
-            prev_min = min(prev_min, prices[i])
+        @lru_cache(maxsize=None)
+        def solve(pos, t, is_bought):
+            if pos >= n or t == 0:
+               return 0
+            result = solve(pos + 1, t, is_bought)
+            if is_bought:
+                result = max(result, solve(pos + 1, t - 1, False) + prices[pos])
+            else:
+                result = max(result, solve(pos + 1, t, True) - prices[pos])
+            return result
+        return solve(0, 2, False)
+            
 
-        ans = dp[n-1]
-        prev_max = prices[n-1]
-        for i in range(n-3, -1, -1):
-            ans = max(ans, prev_max - prices[i+1] + dp[i])
-            prev_max = max(prev_max, prices[i+1])
-        
-        return ans    
