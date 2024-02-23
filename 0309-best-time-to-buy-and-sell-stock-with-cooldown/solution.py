e7a4c14bb199c91c5407a402821cc46459f24b90
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-02-23 07:34:24 +0000

    Sync LeetCode submission Runtime - 41 ms (80.58%), Memory - 20.6 MB (12.24%)

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.py
@@ -1,17 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        if n <= 1:
-            return 0
+        @lru_cache(maxsize=None)
+        def solve(pos, is_bought):
+            if pos >= n:
+               return 0
+            result = solve(pos + 1, is_bought)
+            if is_bought:
+                result = max(result, solve(pos + 2, False) + prices[pos])
+            else:
+                result = max(result, solve(pos + 1, True) - prices[pos])
+            return result
 
-        have_stock_at_index = [0]*n # represent that at given index I have a stock
-        no_stock_at_index= [0]*n # represent that at given index I don't have a stock
-        have_stock_at_index[0] = -prices[0]
-        no_stock_at_index[0] = 0
-        
-        for i in range(1, n):
-            have_stock_at_index[i] = max(no_stock_at_index[i-2] - prices[i], have_stock_at_index[i-1])
-            no_stock_at_index[i] = max(prices[i] + have_stock_at_index[i-1], no_stock_at_index[i-1])
-
-        return no_stock_at_index[n-1]
+        return solve(0, False)
+            
 
