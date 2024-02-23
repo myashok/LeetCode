4b9939732339313e40855de632bb9733ea066536
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-02-23 07:37:46 +0000

    Sync LeetCode submission Runtime - 920 ms (24.97%), Memory - 232.3 MB (6.52%)

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/solution.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        min_buy_price = prices[0] + fee
-        ans = 0
-        for p in prices:
-            if p > min_buy_price:
-                ans += p - min_buy_price
-                min_buy_price = p
-            elif p + fee < min_buy_price:
-                min_buy_price = p + fee
-
-        return ans
-
-
-
+        n = len(prices)
+        @lru_cache(maxsize=None)
+        def solve(pos, is_bought):
+            if pos >= n:
+               return 0
+            result = solve(pos + 1, is_bought)
+            if is_bought:
+                result = max(result, solve(pos + 1, False) + prices[pos] - fee)
+            else:
+                result = max(result, solve(pos + 1, True) - prices[pos])
+            return result
+        return solve(0, False)
+            
 
