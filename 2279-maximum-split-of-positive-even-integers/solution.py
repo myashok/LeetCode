11484829f3bde1ebd4c0a7ba34858a130ef0f9ca
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-02-26 07:35:51 +0000

    Sync LeetCode submission Runtime - 303 ms (31.10%), Memory - 29.1 MB (78.74%)

--- 2279-maximum-split-of-positive-even-integers/solution.py
+++ 2279-maximum-split-of-positive-even-integers/solution.py
@@ -2,15 +2,12 @@ class Solution:
     def maximumEvenSplit(self, finalSum: int) -> List[int]:
         if finalSum & 1:
             return []
-        first_num = curr_sum = 2
+        num = 2
+        curr_sum = 0
         ans = []
-        ans.append(first_num)
-        while curr_sum < finalSum:
-            first_num += 2
-            if curr_sum + first_num <= finalSum:
-                ans.append(first_num)
-                curr_sum += first_num
-            else:
-                ans[-1] += (finalSum - curr_sum)
-                break
+        while curr_sum + num <= finalSum:
+            ans.append(num)
+            curr_sum += num
+            num += 2
+        ans[-1] += finalSum - curr_sum 
         return ans
