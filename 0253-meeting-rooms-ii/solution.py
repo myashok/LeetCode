3021db46ef9470939a65d54e32e76d910597d5fa
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-02-28 05:33:44 +0000

    Sync LeetCode submission Runtime - 75 ms (56.79%), Memory - 19.9 MB (71.03%)

---
+++ 0253-meeting-rooms-ii/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        if not intervals:
+            return 0
+        free_rooms = []
+        intervals.sort(key= lambda x: x[0])
+        heapq.heappush(free_rooms, intervals[0][1])
+        for i in intervals[1:]:
+            if free_rooms[0] <= i[0]:
+                heapq.heappushpop(free_rooms, i[1])
+            else:
+                heapq.heappush(free_rooms, i[1])
+
+        return len(free_rooms)
