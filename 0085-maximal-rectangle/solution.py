f81807e3f7c8ce153d7ef58180a5f9ff8b25241f
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-04-13 20:07:37 +0000

    Sync LeetCode submission Runtime - 217 ms (63.94%), Memory - 17.9 MB (76.10%)

--- 0085-maximal-rectangle/solution.py
+++ 0085-maximal-rectangle/solution.py
@@ -4,36 +4,23 @@ class Solution:
             return 0
         m, n = len(matrix), len(matrix[0])
         height = [0] * n
-        left = [0] * n
-        right = [n] * n
+
         max_area = 0
         
         for i in range(m):
-            cur_left, cur_right = 0, n
-            # Update height array
+            st = []
             for j in range(n):
                 if matrix[i][j] == '1':
                     height[j] += 1
                 else:
                     height[j] = 0
-            
-            # Update left array
-            for j in range(n):
-                if matrix[i][j] == '1':
-                    left[j] = max(left[j], cur_left)
-                else:
-                    left[j], cur_left = 0, j + 1
-            
-            # Update right array
-            for j in range(n - 1, -1, -1):
-                if matrix[i][j] == '1':
-                    right[j] = min(right[j], cur_right)
-                else:
-                    right[j], cur_right = n, j
-            
-            # Update max area
-            for j in range(n):
-                max_area = max(max_area, height[j] * (right[j] - left[j]))
-        
+                while st and height[st[-1]]>= height[j]:
+                    h = height[st.pop()]
+                    w = j - st[-1] - 1 if st else j
+                    max_area = max(max_area, h*w)
+                st.append(j)
+            while st:
+                h = height[st.pop()]
+                w = n - st[-1] - 1 if st else n
+                max_area = max(max_area, h*w)
         return max_area
-
