4f26f73f401675fbbddd8cb0214571e0cce63092
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-05-16 15:40:58 +0000

    Sync LeetCode submission Runtime - 340 ms (88.80%), Memory - 23.2 MB (91.54%)

---
+++ 0917-boats-to-save-people/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        i, j = 0, len(people) - 1
+        used_boats = 0
+        while i <= j:
+            if people[i] + people[j] <= limit:
+                i += 1
+            j -= 1
+            used_boats += 1
+        return used_boats
+    
