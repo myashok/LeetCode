18ed63b3ae7f2ef94ecbec942435b14c7a156906
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-05-21 03:42:48 +0000

    Sync LeetCode submission Runtime - 37 ms (89.17%), Memory - 16.9 MB (22.38%)

--- 0010-regular-expression-matching/solution.py
+++ 0010-regular-expression-matching/solution.py
@@ -1,18 +1,20 @@
-from functools import lru_cache
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         
         @lru_cache(None)
-        def match(si: int, pi: int) -> bool:
+        def match(s, si, p, pi):
             if pi < 0:
-                return si < 0    
+                return si < 0
             if si < 0:
-                return p[pi] == '*' and match(si, pi-2)      
+                return all([True if p[i] =='*' else False for i in range(pi, -1, -2)])
             
+            if p[pi] == '.' or p[pi] == s[si]:
+                return match(s, si-1, p, pi-1)
+
             if p[pi] == '*':
-                return (p[pi-1] in (s[si], '.') and match(si-1, pi)) or match(si, pi-2)
-            else:
-                return (p[pi] in (s[si], '.')) and match(si-1, pi-1)
-            
-        return match(len(s)-1, len(p)-1)
-   
+                return match(s, si, p, pi - 2) or (p[pi-1] in (s[si], '.') and match(s, si - 1, p, pi))
+            return False
+
+        return match(s, len(s) - 1, p, len(p) - 1)
+
+
