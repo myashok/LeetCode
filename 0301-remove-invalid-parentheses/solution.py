c94c09e393829649aad129146ea0431c1dda1d92
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-05-23 03:07:53 +0000

    Sync LeetCode submission Runtime - 129 ms (63.21%), Memory - 17.2 MB (29.19%)

--- 0301-remove-invalid-parentheses/solution.py
+++ 0301-remove-invalid-parentheses/solution.py
@@ -1,57 +1,40 @@
-# Objective is to find valid string with least number of changes.
-# If we use BFS and traverse levels, where levels represent number of changes/removals in the string then we will be able to find the valid strings corresponding to each level.
-#Eg.
-# starting = "()())()"
-# Level 0 -> no valid string
-# Level 1 -> "()()()", "(())()"
-# Level 2 -> no valid string
-# Level 3 -> "()()", "(())"
-# In our case we want to minimize the change, so we will stop traversing into additional levels whenever we encounter a valid string in a level. In the above example we encounter valid string in Level 1, so we can stop traversing any further and just explore that same level to find out more valid strings.
+from collections import deque
+from typing import List
 
 class Solution:
-    # To identify if the char is a parenthesis
     def isParenthesis(self, c):
-        if c in set(["(", ")"]):
-            return True
-        return False
+        return c in {"(", ")"}
     
-    # To check if the string is valid or not. Matching parenthesis
     def isValid(self, candidate):
-            counter = 0
-            for char in candidate:
-                if char == "(": counter += 1
-                elif char == ")": counter -= 1
-                if counter < 0: return False
-            # balanced?
-            return counter == 0
-     
-                
-    
+        counter = 0
+        for char in candidate:
+            if char == "(": counter += 1
+            elif char == ")": counter -= 1
+            if counter < 0: return False
+        return counter == 0
     
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        queue = [s]
-        seen = set()
-        
+        queue = deque([s])
+        seen = set([s])
         result = []
-        dont_traverse = False
-        while(queue):
-            str = queue.pop(0)
-            if str not in seen:
-                seen.add(str)
-            else:
+        found = False
+
+        while queue:
+            current = queue.popleft()
+            
+            if self.isValid(current):
+                result.append(current)
+                found = True
+            
+            if found:
                 continue
-            # Find if it is a valid string
-            if self.isValid(str):
-                # Add this to result and set the flag to stop traversing any further vertically.
-                result.append(str)
-                dont_traverse = True
             
-            
-            if not dont_traverse: 
-                for i,p in enumerate(str):
-                    if self.isParenthesis(p):
-                        temp_str = str[0:i] + str[i+1:]
+            for i in range(len(current)):
+                if self.isParenthesis(current[i]):
+                    temp_str = current[:i] + current[i+1:]
+                    if temp_str not in seen:
                         queue.append(temp_str)
+                        seen.add(temp_str)
         
-            
-        return result        
+        return result if result else [""]
+
