8f473ab29df2b1bc9739b2aa4e7c24178cb86133
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-05-24 03:34:43 +0000

    Sync LeetCode submission Runtime - 50 ms (73.77%), Memory - 16.6 MB (96.33%)

---
+++ 0047-permutations-ii/solution.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        used = [False] * len(nums)
+        
+        def solve(path):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue
+                used[i] = True
+                path.append(nums[i])
+                solve(path)
+                path.pop()
+                used[i] = False
+        
+        solve([])
+        return res
+
