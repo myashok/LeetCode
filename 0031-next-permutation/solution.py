bafa4cb4474d17e4d196d651e6b7069392bc3ef2
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-05-27 04:55:12 +0000

    Sync LeetCode submission Runtime - 50 ms (12.00%), Memory - 16.4 MB (91.94%)

--- 0031-next-permutation/solution.py
+++ 0031-next-permutation/solution.py
@@ -4,15 +4,21 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
+        flagged_index = -1
         for i in range(n-2, -1, -1):
+            # print(i, nums[i], nums[i+1])
             if nums[i] < nums[i+1]:
-                nums[i+1:] = nums[i+1:][::-1]
-                for j in range(i + 1, n):
-                    if nums[j] > nums[i]:
-                        nums[j], nums[i] = nums[i], nums[j]
-                        return
-
-        nums.sort()
+                flagged_index = i
+                break
+        # print(flagged_index)
+        if flagged_index != -1:
+            for i in range(n-1, flagged_index, -1):
+                if nums[i] > nums[flagged_index]:
+                    nums[i], nums[flagged_index] = nums[flagged_index], nums[i]
+                    nums[flagged_index+1:] = sorted(nums[flagged_index+1:])
+                    break
+        else:
+            nums.sort()
 
             
 
