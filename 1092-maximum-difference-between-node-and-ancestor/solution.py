705302aab5e319cab8ed7fd1f927e7df3da3443e
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-05-27 04:49:54 +0000

    Sync LeetCode submission Runtime - 40 ms (52.07%), Memory - 17.9 MB (94.53%)

--- 1092-maximum-difference-between-node-and-ancestor/solution.py
+++ 1092-maximum-difference-between-node-and-ancestor/solution.py
@@ -1,18 +1,25 @@
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
+#         self.val = val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
-    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
-        def traverse(root, mx, mn, parent):
-            if not root:
-                return 0
-            node_max = 0
-            if parent != None:
-               node_max = max(abs(mx - root.val), abs(mn - root.val))  
-            left_max = traverse(root.left, max(mx, root.val), min(mn, root.val), root)
-            right_max = traverse(root.right, max(mx, root.val), min(mn, root.val), root)
-            return max(node_max, left_max, right_max)
-        return traverse(root, 0, 10**5+1, None)
+    def maxAncestorDiff(self, node: Optional[TreeNode], curr_min = 100000, curr_max = 0) -> int:
+        if not node:
+            return curr_max - curr_min
+
+        curr_min = min(curr_min, node.val)
+        curr_max = max(curr_max, node.val)
+        
+        # Recurse for left and right children
+        left_diff = self.maxAncestorDiff(node.left, curr_min, curr_max)
+        right_diff = self.maxAncestorDiff(node.right, curr_min, curr_max)
+        
+        # Return the maximum difference found in the subtree rooted at this node
+        return max(left_diff, right_diff)
+
+    
+    
+
