17c001aa6c77b9d013ea727e45666b27d4740d41
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-05-28 03:54:56 +0000

    Sync LeetCode submission Runtime - 166 ms (87.94%), Memory - 20.1 MB (79.26%)

---
+++ 0721-accounts-merge/solution.py
@@ -0,0 +1,42 @@
+class UF:
+    def __init__(self, N):
+        self.parents = list(range(N))
+        self.rank = defaultdict(int)
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        rank = self.rank
+        parent = self.parents
+        if root_x != root_y:
+            if rank[root_x] > rank[root_y]:
+                parent[root_y] = root_x
+            elif rank[root_x] < rank[root_y]:
+                parent[root_x] = root_y
+            else:
+                parent[root_y] = root_x
+                rank[root_x] += 1 
+
+    def find(self, x):
+        if x != self.parents[x]:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+    
+class Solution:
+    # 196 ms, 82.09%. 
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UF(len(accounts))
+        
+        # Creat unions between indexes
+        ownership = {}
+        for i, (_, *emails) in enumerate(accounts):
+            for email in emails:
+                if email in ownership:
+                    uf.union(i, ownership[email])
+                ownership[email] = i
+        
+        # Append emails to correct index
+        ans = collections.defaultdict(list)
+        for email, owner in ownership.items():
+            ans[uf.find(owner)].append(email)
+        
+        return [[accounts[i][0]] + sorted(emails) for i, emails in ans.items()]
