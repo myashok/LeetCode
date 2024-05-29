bcf035f572b69571bd4e5c2f8f05f2c5a7e43652
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-05-29 16:57:19 +0000

    Sync LeetCode submission Runtime - 129 ms (48.32%), Memory - 17.3 MB (78.49%)

--- 0076-minimum-window-substring/solution.py
+++ 0076-minimum-window-substring/solution.py
@@ -1,19 +1,22 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(s) < len(t):
-            return ""           
-        count, l, r = Counter(t), 0, 0
-        ans_l, ans_r,  n, total_count = 0, 10**5+1, len(s), len(t)
+            return ""
+        count_t = Counter(t)
+        required_count = len(t)
+        ans_l, ans_r = 0, 10**5 + 1
+        l = r = 0
+        n = len(s)
         while r < n:
-            total_count -= (count[s[r]] > 0)
-            count[s[r]] -= 1
-            r += 1
-            while total_count == 0:
+            required_count -= count_t[s[r]] > 0
+            count_t[s[r]] -= 1
+            while required_count == 0:
                 if ans_r - ans_l > r - l:
                     ans_r, ans_l = r, l
-                total_count += (count[s[l]] == 0)
-                count[s[l]] += 1   
-                l += 1
+                required_count += count_t[s[l]] == 0
+                count_t[s[l]] += 1
+                l += 1      
+            r += 1
 
-        return "" if ans_r == 10**5+1 else s[ans_l: ans_r]
-            
+        return "" if ans_r == 10**5 + 1 else s[ans_l: ans_r + 1]
+
