9dd2831298c2ba6cae83ac1031c139822814a30d
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-06-01 11:33:13 +0000

    Sync LeetCode submission Runtime - 94 ms (95.50%), Memory - 17.1 MB (70.42%)

--- 0695-max-area-of-island/solution.py
+++ 0695-max-area-of-island/solution.py
@@ -2,22 +2,22 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         rows = len(grid)
         cols = len(grid[0])
-        visited = set()
-        old_visited_count = 0
         ans = 0
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         def dfs(r, c):
-            directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-            visited.add((r, c))
+            grid[r][c] = index
+            res = 0
             for dx, dy in directions:
                 new_r = dx + r
                 new_c = dy + c
-                if 0 <= new_r < rows and 0 <= new_c < cols and (new_r, new_c) not in visited and grid[new_r][new_c] == 1:
-                    dfs(new_r, new_c)
-
+                if 0 <= new_r < rows and 0 <= new_c < cols and grid[new_r][new_c] == 1:
+                    res += dfs(new_r, new_c)
+            return res + 1
+        index = 2
         for i in range(rows):
             for j in range(cols):
-                if (i, j) not in visited and grid[i][j]:
-                    dfs(i, j)
-                    ans = max(ans, len(visited) - old_visited_count)
-                    old_visited_count = len(visited) 
+                if grid[i][j] == 1:
+                    ans = max(ans, dfs(i, j))
+                    index += 1
         return ans
+                    
