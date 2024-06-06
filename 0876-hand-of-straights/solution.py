a67bfc998074d6e74be5467dbbc376f70334dd55
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-06-06 13:11:54 +0000

    Sync LeetCode submission Runtime - 157 ms (78.18%), Memory - 18.5 MB (50.76%)

--- 0876-hand-of-straights/solution.py
+++ 0876-hand-of-straights/solution.py
@@ -1,24 +1,23 @@
-from sortedcontainers import SortedDict
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        n = len(hand)
-        if n % groupSize != 0:
+        if len(hand) % groupSize != 0:
             return False
-        c = Counter(hand)
-        min_heap = list(c.keys())
-        heapq.heapify(min_heap)
-        while min_heap:
-            smallest = min_heap[0]
-            for i in range(smallest, smallest+groupSize):
-                if c[i] == 0:
+
+        # Counter to store the count of each card value
+        card_count = Counter(hand)
+
+        for card in hand:
+            start_card = card
+            if card_count[start_card] == 0:
+                continue
+            
+            # Find the start of the potential straight sequence
+            while card_count[start_card - 1]:
+                start_card -= 1
+
+            for next_card in range(start_card, start_card + groupSize):
+                if not card_count[next_card]:
                     return False
+                card_count[next_card] -= 1
 
-                if c[i] == 1:
-                    if i != min_heap[0]:
-                        return False
-                    heapq.heappop(min_heap)
-                c[i] -= 1
         return True
-
-       
-
