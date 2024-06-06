7a5fdd71659b2f06131628cf0812c7c852b1978c
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-06-06 17:19:27 +0000

    Sync LeetCode submission Runtime - 44 ms (90.35%), Memory - 16.7 MB (30.69%)

--- 0040-combination-sum-ii/solution.py
+++ 0040-combination-sum-ii/solution.py
@@ -13,11 +13,12 @@ class Solution:
                 return
             if remain < 0 or start >= n:
                 return
-            if candidates[start] <= target:
-                # Choose the current element
-                comb.append(candidates[start])
-                solve(start + 1, remain - candidates[start])
-                comb.pop()
+            if candidates[start] > remain:
+                return
+            # Choose the current element
+            comb.append(candidates[start])
+            solve(start + 1, remain - candidates[start])
+            comb.pop()
 
             # Skip duplicates and not choose the current element
             while start + 1 < n and candidates[start] == candidates[start + 1]:
