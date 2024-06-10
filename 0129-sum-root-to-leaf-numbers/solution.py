b36eacf54f66d056a555c525014657c32bef8b59
Shiv Pratap Singh <iit2014121@iiita.ac.in>
2024-06-10 15:42:04 +0000

    Sync LeetCode submission Runtime - 30 ms (88.46%), Memory - 16.3 MB (97.14%)

--- 0129-sum-root-to-leaf-numbers/solution.py
+++ 0129-sum-root-to-leaf-numbers/solution.py
@@ -4,25 +4,13 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        total_sum = 0
-        
-        def traverse(node, curr_sum):
-            nonlocal total_sum
-            if not node:
-                return
-                
-            curr_sum = curr_sum * 10 + node.val
-            
-            if not node.left and not node.right:
-                total_sum += curr_sum
-                return
-
-            traverse(node.left, curr_sum)
-            traverse(node.right, curr_sum)
-        
-        traverse(root, 0)
-        return total_sum
-
+        def dfs(curr,num):
+            if curr is None:
+                return 0
+            num=num*10+curr.val
+            if not curr.left  and not curr.right:
+                return num
+            return dfs(curr.left, num)+dfs(curr.right, num)
+        return dfs(root,0)
